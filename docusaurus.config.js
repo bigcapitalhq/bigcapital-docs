41aba29d4016740518eaa6861262b4f253fe8504
a.bouhuolia <a.bouhuolia@live.com>
2022-03-13 21:04:16 +0200

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,143 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Bigcapital Learning Central',
+  tagline:
+    'Learn the skills to build your business using Bigcapital accounting software.',
+  url: 'https://docs.bigcapital.ly',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon-32x32.png',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'bigcapital-docs', // Usually your repo name.
+  stylesheets: [
+    'https://fonts.googleapis.com/css?family=Inter:400,500,600,800&amp;display=swap',
+  ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ar'],
+    localeConfigs: {
+      en: {
+        label: 'English',
+        direction: 'ltr',
+        htmlLang: 'en-US',
+      },
+      ar: {
+        label: 'Arabic',
+        direction: 'rtl',
+        htmlLang: 'ar',
+      },
+    },
+  },
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Bigcapital',
+        // logo: {
+        // alt: 'My Site Logo',
+        // src: 'img/logo.svg',
+        // },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'FAQs',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            type: 'localeDropdown',
+            position: 'right'
+          }
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+              {
+                label: 'FAQs',
+                to: '/docs/faqs',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Facebook',
+                href: 'https://facebook/bigcapitalHQ',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/bigcapitalHQ',
+              },
+              {
+                href: 'https://github.com/bigcapitalHQ',
+                label: 'GitHub',
+                position: 'right',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Bigcapital, Inc. Built by Bigcapital Technologies.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
